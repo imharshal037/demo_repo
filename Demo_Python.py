74b9839341f9db9b05a4a2fa06870d9072d0d53b
imharshal037 <harshal.patil037@example.com>
2025-11-23 16:10:48 +0530

    Added the older commit git cherry-pick 6e1d146c76546aee0b6d6245d11f1aa46593dddd

--- Demo_Python.py
+++ Demo_Python.py
@@ -1,9 +1,1 @@
 print("Wel Come To Python Programming")
-print("This is remote changes")
-
-a = 10
-b = 20
-c = a + b
-print("Sum of a and b is:", c)
-print("This line is added in local repository")
-print("This is latest changes of remote repository")
