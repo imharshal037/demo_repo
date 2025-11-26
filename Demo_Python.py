07851c681063cebfe2e26d2b1ca85c5347a0405a
imharshal037 <harshal.patil037@example.com>
2025-11-26 23:15:14 +0530

    Added the addition logic

--- Demo_Python.py
+++ Demo_Python.py
@@ -1,2 +1,8 @@
 print("Wel Come To Python Programming")
 print("This is remote changes")
+
+a = 10
+b = 20
+c = a + b
+print("Sum of a and b is:", c)
+print("This line is added in local repository")
